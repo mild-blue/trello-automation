6cec83074607a0904d2a9b3b4d6e49865979f1b8
Matej Kloucek <matej.kloucek@mild.blue>
2022-11-08 19:32:36 +0100

    Made source_card_ids and all_source_card_ids sets to avoid duplicate cards

--- main.py
+++ main.py
@@ -38,12 +38,11 @@ def search_board(searched_board_id: int, target_list_id: int = DEFAULT_TARGET_LI
 def search_list(searched_list_id: int, target_list_id: int = DEFAULT_TARGET_LIST_ID):
     response = make_trello_request(f"lists/{searched_list_id}/cards")
     cards_on_list = json.loads(response.text)
-    source_card_ids = []
+    source_card_ids = set()
     for card in cards_on_list:
         for name in MEMBER_NAME_ID_PAIRS:
             if (MEMBER_NAME_ID_PAIRS[name] in card['idMembers']) and (check_due_date(card['id'])):
-                source_card_ids.append(card['id'])
-                break
+                source_card_ids.add(card['id'])
     return source_card_ids
 
 
@@ -103,8 +102,8 @@ if __name__ == '__main__':
     all_source_list_ids = []
     for board_id in BOARD_IDS:
         all_source_list_ids = all_source_list_ids + search_board(board_id)
-    all_source_card_ids = []
+    all_source_card_ids = set()
     for source_list_id in all_source_list_ids:
-        all_source_card_ids = all_source_card_ids + search_list(source_list_id)
+        all_source_card_ids.update(search_list(source_list_id))
     for source_card_id in all_source_card_ids:
         copy_card(source_card_id, DEFAULT_TARGET_LIST_ID)
